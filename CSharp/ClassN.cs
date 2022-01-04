a239f5fd2fb6cc417c970d74e453a8b84ee91c4f
Magno Carneiro <magnovaldo.carneiro@gmail.com>
2022-01-04 17:43:37 -0300

    Add project files.

---
+++ CSharp/ClassN.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp
+{
+    internal class Pessoa
+    {
+        public string Nome { get; set; }
+        public string Estado { get; set;}
+        public int Idade { get; set; }
+    }
+}
---
+++ CSharp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CSharp;
+using Interface;
+
+namespace CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa person = new Pessoa();
+
+            person.Nome = "Magno";
+            person.Idade = 54;
+            person.Estado = "SP";
+
+            Animal animal = new Animal();
+            animal.Nome = "Rex";
+            animal.Dono = "Magno";
+            animal.Especie = "Cachorro";
+          
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
